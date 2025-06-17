594ba64dcbf9b641537c6de311dbbb2b88c76925
anujupadhyay123 <anuju2024@gmail.com>
2025-06-17 14:53:09 +0530

    added: LangChain5

---
+++ LangChain5/Interview Question Creator/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="Anuj Upadhyay",
+    author_email="anuju760@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
